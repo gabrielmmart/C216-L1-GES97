88805ba25047e94fcb5784122db56137fcf0d2b3
gabrielmmart <gabriel.medeiros@ges.inatel.br>
2024-03-09 21:23:25 -0300

    Initial commit

---
+++ pratica-0/src/index.js
@@ -0,0 +1,8 @@
+ nome = "Gabriel Medeiros - 97";
+function saudacaoPersonalizada(nome) {
+    console.log("Eh os guri, " + nome + " :)");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+    mensagem: 'Testando modulo: Gabriel Medeiros - 97 !',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
